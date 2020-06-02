78a3658ef040390757e56bfea5b032fd82572356
Jacob Desselles <jacob.desselles@gmail.com>
2020-06-02 18:20:17 -0500

    forst tiemz comittin thru ubuntu uwu

--- dispy.py
+++ dispy.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import numpy as np
 
-df = [20,0,12,-20,40, np.nan, np.nan]
+df = [20,0,12,-20,40, 'NaN', 'NaN']
 null = np.NaN
 def isitnan(df):
     for x in df:
-        if null:
+        if x == 'NaN':
             print('FALSE')
         else:
             print(x)
 isitnan(df)
+
+def splitdate(df):
+    for x in df:
+        df['day'] = df.index.day
+        df['month'] = df.index.month
+        df['year'] = df.index.year
