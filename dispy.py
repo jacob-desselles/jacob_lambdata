63e19e7430aad5e705473ed885a653213112a73f
Jacob Desselles <jacob.desselles@gmail.com>
2020-06-04 01:47:24 -0500

    added classes

--- dispy.py
+++ dispy.py
@@ -3,19 +3,21 @@ import numpy as np
 
 df = [20,0,12,-20,40, 'NaN', 'NaN']
 null = np.NaN
-def isitnan(df):
+class NanScan:
+    def isitnan(df):
     #Will scan any dataset for NaNs and will replace with 'FALSE', but it will first have to convert
     #NaN values to strings.
-    for x in df:
-        if x == 'NaN':
-            print('FALSE')
-        else:
-            print(x)
+        for x in df:
+            if x == 'NaN':
+                print('FALSE')
+            else:
+                print(x)
 isitnan(df)
+class DMY:
 
-def splitdate(df):
-    #Will split any timedate-format columns into three separate columns.
-    for x in df:
-     df['day'] = df['datetime_utc'].dt.day
-     df['month'] = df['datetime_utc'].dt.month
-     df['year'] = df['datetime_utc'].dt.year
+    def splitdate(df):
+        #Will split any timedate-format columns into three separate columns.
+        for x in df:
+            df['day'] = df['datetime_utc'].dt.day
+            df['month'] = df['datetime_utc'].dt.month
+            df['year'] = df['datetime_utc'].dt.year
