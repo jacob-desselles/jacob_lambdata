bbcb3f8d8a748683d0174324bab2345915e13a3b
Jacob Desselles <jacob.desselles@gmail.com>
2020-06-03 20:15:39 -0500

    Some minor comments and adjustments

--- dispy.py
+++ dispy.py
@@ -4,6 +4,8 @@ import numpy as np
 df = [20,0,12,-20,40, 'NaN', 'NaN']
 null = np.NaN
 def isitnan(df):
+    #Will scan any dataset for NaNs and will replace with 'FALSE', but it will first have to convert
+    #NaN values to strings.
     for x in df:
         if x == 'NaN':
             print('FALSE')
@@ -12,6 +14,7 @@ def isitnan(df):
 isitnan(df)
 
 def splitdate(df):
+    #Will split any timedate-format columns into three separate columns.
     for x in df:
         df['day'] = df.index.day
         df['month'] = df.index.month
--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-shuulaces", # the name that you will install via pip
-    version="1.0",
+    version="1.1",
     author="Jacob Desselles",
     author_email="jacob.desselles@gmail.com",
     description="NaN identifier and date separater",
-    long_description='Will only scan NaNs if string, will also separate any date time objects into three different columns',
+    long_description='Will only scan NaNs if string, will also separate any date time objects into three different columns. To call functions, use isitnan(df) and splitdate(df).',
     long_description_content_type='text/markdown',# required if using a md file for long desc
     #license="MIT",
     url="https://github.com/jacob-desselles/jacob_lambdata",
